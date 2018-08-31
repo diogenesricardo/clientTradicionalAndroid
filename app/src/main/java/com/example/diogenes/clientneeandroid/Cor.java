942873b13f8a80fc92879ed084fe58402cbd3666
diogenesricardo <diogenesr.freitas@gmail.com>
2018-08-31 17:28:59 -0300

    postSample with OkHttp

---
+++ app/src/main/java/com/example/diogenes/clientneeandroid/Cor.java
@@ -0,0 +1,10 @@
+package com.example.diogenes.clientneeandroid;
+
+class Cor {
+
+    private String descricao;
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
--- app/src/main/java/com/example/diogenes/clientneeandroid/MainActivity.java
+++ app/src/main/java/com/example/diogenes/clientneeandroid/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.diogenes.clientneeandroid;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.JsonReader;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +17,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSample();
+//        getSample();
+
+        try {
+            postSample();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-    private void getSample(){
+    private void getSample() {
 
         AsyncTask.execute(new Runnable() {
             @Override
@@ -83,61 +100,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void post(){
-        AsyncTask.execute(new Runnable() {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    private void postSample() throws IOException {
+
+        String url = "http://192.168.137.1:8080/cores";
+
+        Cor cor = new Cor();
+        cor.setDescricao("Branco Neve");
+
+        Gson gson = new Gson();
+        String jsonObject = gson.toJson(cor);
+
+
+        RequestBody body = RequestBody.create(JSON, jsonObject);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
             @Override
-            public void run() {
-                // All your networking logic
-                // should be here
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
 
-                try {
-                    // 01 - Create URL
-                    URL getInsumosURL = new URL("http://192.168.137.1:8080/insumos");
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
 
-                    // 02 - Create connection
-                    HttpURLConnection myConnection =
-                            (HttpURLConnection) getInsumosURL.openConnection();
+                String mMessage = response.body().string();
+                if (response.isSuccessful()){
+                    try {
+                        JSONObject json = new JSONObject(mMessage);
+                        final String serverResponse = json.getString("descricao");
 
-                    // 03 - InputStream do corpo da resposta
-                    InputStream responseBody = myConnection.getInputStream();
-
-                    // 04 - Leitor do Stream
-                    InputStreamReader responseBodyReader =
-                            new InputStreamReader(responseBody, "UTF-8");
-
-                    // 05 - Leitor de JSON
-                    JsonReader jsonReader = new JsonReader(responseBodyReader);
-
-                    // 06 - Iterar
-                    jsonReader.beginObject(); // Start processing the JSON object
-                    while (jsonReader.hasNext()) { // Loop through all keys
-                        String key = jsonReader.nextName(); // Fetch the next key
-                        if (key.equals("totalElements")) { // Check if desired key
-                            // Fetch the value as a String
-                            String value = jsonReader.nextString();
-
-                            // Do something with the value
-                            // ...
-                            TextView total = (TextView) findViewById(R.id.total);
-                            total.setText(value);
-
-                            break; // Break out of the loop
-                        } else {
-                            jsonReader.skipValue(); // Skip values of other keys
-                        }
+                    } catch (Exception e){
+                        e.printStackTrace();
                     }
 
-                    // 07 - Fechando o leitor de JSON
-                    jsonReader.close();
-
-                    // 08 - Fechando a conexão
-                    myConnection.disconnect();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }
         });
+
     }
+
+
 
 
 }
